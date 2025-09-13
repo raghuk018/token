0e264b2481e09a1337f8c13977039aa70c9a5a76
ragu <kanikarmraghuvarma@gmail.com>
2025-09-13 16:22:11 +0530

    eee

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,11 +6,6 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 const createPrismaClient = () => {
   return new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
   });
 };
 
